e86e39ec40a4ade3d7194c1f6e2667a05ed78355
Leonildo Camilo <leonildo_camilo@hotmail.com>
2024-01-07 12:24:52 -0300

    Complete Application

--- index.js
+++ index.js
@@ -74,6 +74,58 @@ app.get('/books/:id', (req, res) =>{
   })
 })
 
+app.get('/books/edit/:id', (req, res) => {
+  const id = req.params.id
+
+  const sql = `SELECT * FROM books WHERE id = ${id}`
+
+  conn.query(sql, function (err, data) {
+    if (err) {
+      console.log(err)
+      return
+    }
+
+    const book = data[0]
+
+    res.render('editbook', {book})
+})
+
+})
+
+app.post('/books/updatebook', (req, res) => {
+
+  const id = req.body.id
+  const title = req.body.title
+  const pageqty = req.body.pageqty
+
+  const sql = `UPDATE books SET title = '${title}', pageqty = '${pageqty}' WHERE id = ${id}`
+
+  conn.query(sql, function(err){
+    if(err){
+      console.log(err)
+      return
+    }
+
+    res.redirect('/books')
+  })
+})
+
+app.post('/books/remove/:id', (req, res) => {
+  
+  const id = req.params.id
+
+  const sql = `DELETE FROM books WHERE id = ${id}`
+
+  conn.query(sql, function(err){
+    if(err){
+      console.log(err)
+      return
+    }
+
+    res.redirect('/books')
+  })
+})
+
 //conecta a databse do banco de dados
 const conn = mysql.createConnection({
   host: 'localhost',
